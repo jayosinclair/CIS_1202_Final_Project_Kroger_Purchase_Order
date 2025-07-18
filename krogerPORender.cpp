559ffbfb20a56b55bbe8a8e126ab8acb96c996fc
Jay Olson <jay.olson@sinclair.edu>
2025-07-17 21:36:40 -0400

    Add project files.

---
+++ krogerPORender.cpp
@@ -0,0 +1,13 @@
+//Jay Olson
+//CIS 1202-800
+//July 17, 2025
+//Final Project: Kroger Purchase Order "Renderer"
+
+//This project takes a sample, dumbed-down purchase order ANSI X12 EDI 850 transaction using Kroger's published implementation convention
+//and "renders" it for mortals to read both in the console and in a text file. This program implements concepts from CIS 1202 in the process:
+//Sorting algorithms, character data, structs, enums, classes, exceptions, and templates.
+//Note: I did not attempt to implement the entire implementation convention, and I'm sure if I knew more there would be better ways of doing
+//things. This is an elementary stab at it.
+
+//Kroger's implementation convention is published at https://edi.kroger.com/EDIPortal/documents/Maps/kroger-modernized-systems/Kroger-Modernized-systems_EDI850.pdf.
+//See the readme file on GitHub repository for this project for some EDI basics as they apply to this project.
